d807f608c60835846a70b8e7e240355dbade3da6
Greg Jeffrey <gjeffs11@gmail.com>
2018-04-22 15:52:31 -0400

    Weekly moving windows

--- NNTest.py
+++ NNTest.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[4]:
+# In[71]:
 
 
 import pandas as pd
@@ -13,9 +13,10 @@ from torch.autograd import Variable
 from torch import optim
 import numpy as np
 import math, random
+import matplotlib.pyplot as plt
 
 
-# In[52]:
+# In[72]:
 
 
 data = pd.read_csv('full_data.csv',delimiter=',',header=0,index_col=0)
@@ -30,22 +31,22 @@ net_input_dict = dict({
 
 df = pd.DataFrame(net_input_dict) ##Input data is a 61392x9 matrix
 
-net_inputs = (df - df.mean()) / (df.max() - df.min())
+net_inputs = (df - df.min()) / (df.max() - df.min())
 net_targets = net_inputs['MWh'].values #Targets are a 61392 column vector
-
 net_inputs = net_inputs.values
 
 
-# In[28]:
+
+# In[73]:
 
 
 ##Define the model
 class SimpleRNN(nn.Module):
     def __init__(self, hidden_size,num_features):
         super(SimpleRNN, self).__init__()
+        
         self.hidden_size = hidden_size
         self.num_features = num_features
-        
         self.inp = nn.Linear(num_features, hidden_size) ##Input layer: fully-connected layer, takes in feature #, outputs hidden size
         
         ##Use 1 LSTM layer
@@ -55,7 +56,6 @@ class SimpleRNN(nn.Module):
     #def step(self, input, hidden=None):
         #Input is one step of the sequence, or 1x9
         
-
     def forward(self, input, hidden=None, force=True, steps=0):
         if force or steps == 0: steps = len(inputs) ##Sets the # of steps to the # of inputs
         outputs = Variable(torch.zeros(steps, 1, 1)) ##Initializes the output tensor (row vector of # of steps)
@@ -64,19 +64,16 @@ class SimpleRNN(nn.Module):
         #Input size: (N,1,num_features)
         #Outut size: (N,1,hidden_size)
         lstm_input = self.inp(inputs.view(-1,1,num_features)) ##Applies the data to the input layer, returns input variable
-        print(lstm_input.shape)
         
         #LSTM Layer
         #Input size: (seq_length,batch,input_size)
         #Output size: (seq_length,batch,hidden_size)
         lstm_output, hidden = self.lstm(lstm_input, hidden) ##Applies the input variable to the rnn layer(s), returns output and hidden states
-        print(lstm_output.shape)
         
         #Linear Layer
         #Input size: (N,1,hidden_size)
         #Output size:(N,1,1)
         output = self.out(lstm_output.squeeze(1)) ##Applies the LSTM output to the linear output layer, returns output
-        print(output.shape)
         
         return output, hidden
     
@@ -84,11 +81,11 @@ class SimpleRNN(nn.Module):
 
 
 
-# In[29]:
+# In[74]:
 
 
-n_epochs = 50
-hidden_size = 20
+n_epochs = 1
+hidden_size = 1
 num_features=6
 
 model = SimpleRNN(hidden_size,num_features) #Creates the model defined above
@@ -99,31 +96,36 @@ losses = np.zeros(n_epochs) # #Initializes loss variable, for plotting
 
 
 
-# In[30]:
+# In[ ]:
 
 
+#torch.cuda.get_device_name(0)
 #Network training
 
-inputs = Variable(torch.from_numpy(net_inputs[:-1]).float()) ##Prepares the input sequence
-targets = Variable(torch.from_numpy(net_targets[1:]).float()) ##Prepares the target sequence
+inputs = Variable(torch.from_numpy(net_inputs[:-1]).float())#.cuda
+targets = Variable(torch.from_numpy(net_targets[1:]).float())#.cuda
 
+
+#Prepare weekly moving windows
+sequences = Variable(torch.zeros(len(inputs)-24*7,24*7,6))
+for i in range(0,len(inputs)-24*7):
+    sequences[i,:,:] = inputs[i:i+24*7,:]  
+    
+    
 for epoch in range(n_epochs):
     
-    #We have 1 long sequence as opposed to a bunch of smaller sequences, so inner loop isn't necessary
+    for seq in sequences:
+        outputs, hidden = model(seq, None) #Input is a weekly moving window (168x6)
 
-    # Use teacher forcing 50% of the time
-    force = random.random() < 0.5
-    outputs, hidden = model(inputs, None, force) #Input is a 61392x9 matrix
+        optimizer.zero_grad() ##Clears the gradients
+        loss = criterion(outputs, targets) ##Calculates loss
+        loss.backward() ##Performs backpropagation
+        optimizer.step()
 
-    optimizer.zero_grad() ##Clears the gradients
-    loss = criterion(outputs, targets) ##Calculates loss
-    loss.backward() ##Performs backpropagation
-    optimizer.step()
+        losses[epoch] += loss.data[0]
 
-    losses[epoch] += loss.data[0]
-
-    if epoch > 0:
-        print(epoch, loss.data[0])
+        if epoch > 0:
+            print(epoch, loss.data[0])
 
     # Use some plotting library
     # if epoch % 10 == 0:
@@ -136,17 +138,25 @@ for epoch in range(n_epochs):
         # show_plot('generated', outputs.data.view(-1), True)
 
 
-
-# In[51]:
-
-
-import matplotlib.pyplot as plt
-plt.plot(outputs.data.numpy())
-plt.plot(targets.data.numpy())
+#Test some data
+output = model(seq[-1,:,:],None)
 
 
 # In[54]:
 
 
+#targets = Variable(torch.from_numpy(net_targets[1:]).float())#.cuda
+#plt.plot(targets.data.numpy())
+
+#preds = pd.read_csv('preds.csv')
+#plt.plot(preds['0'])
+
+
+# In[1]:
+
+
+
+
+    
 
 
