862de6907f4f61ef081c65b7071cfeaafadd2102
Naveen Sehgal <naveen.sehg@gmail.com>
2018-04-26 14:51:44 -0400

    try

--- run_lstm.py
+++ run_lstm.py
@@ -7,12 +7,17 @@ import torch.nn as nn
 from torch.autograd import Variable
 from torch import optim
 import numpy as np
+import random
 
 # ---------------------------- PARAMS  ------------------------------- #
 EPOCHS = 1000
 LEARNING_RATE = 0.2
 HIDDEN_DIMS = 25
 NUM_LSTM_CELLS = 1
+NUM_WINDOWS = 5
+WINDOW_LENGTH = 8760  # 1-year windows
+FORECAST_HORIZON = 24
+
 
 # ------------------------ Define Network  --------------------------- #
 class LSTM(torch.nn.Module):
@@ -38,23 +43,28 @@ class LSTM(torch.nn.Module):
 
 
 # ---------------------- Load and Process Data  ---------------------- #
-df = pd.read_csv('full_data.csv', index_col=0)
-cols = ['apparentTemperature', 'humidity',
-        'pressure', 'MWh']
+data = pd.read_csv('full_data.csv', index_col=0)
+cols = ['apparentTemperature', 'humidity', 'MWh']
+df = data[cols]
 
-df = df[cols]
-df['next_pow'] = df.shift(-1).MWh
-df = df[:-1]
+df = (df - df.min())/(df.max()-df.min())  # Min-Max Normalization
 
-# Obtain target data
-y = np.array(df.next_pow.values)
-target = Variable(torch.from_numpy(y)).float().cuda()
+inputs = df
+targets = df['MWh']  # Un-normalized targets
 
-# Normalize input data
-df = (df - df.mean())/df.std()
-x = np.array(df.drop('next_pow', axis=1).values)
-inputs = Variable(torch.from_numpy(x)).float().cuda()
-inputs = inputs.unsqueeze(1)
+# Percentage of samples to use as training data
+TRAINING_SAMPLE_RATIO = 0.7
+num_training_samples = round(len(inputs)*TRAINING_SAMPLE_RATIO)
+
+# Splits data samples
+(training_inputs, test_inputs) = np.split(inputs.values, [num_training_samples])
+(training_targets, test_targets) = np.split(targets.values, [num_training_samples])
+
+# Prepares training data for input to network
+training_inputs = Variable(torch.from_numpy(training_inputs).float()).cuda()
+training_targets = Variable(torch.from_numpy(training_targets).float()).cuda()
+test_inputs = Variable(torch.from_numpy(test_inputs).float()).cuda()
+test_targets = Variable(torch.from_numpy(test_targets).float()).cuda()
 
 # -------------------- Instantiate LSTM Network  --------------------- #
 # Model Params
@@ -71,19 +81,27 @@ losses = []
 
 # Train loop
 for epoch in range(EPOCHS):
-    # Zero gradients
-    optimizer.zero_grad()
+    mse = 0
 
-    # Update weights
-    outputs = model(inputs)
-    loss = criterion(outputs, target)
-    loss.backward()
-    optimizer.step()
+    for i in range(NUM_WINDOWS):
+        index = round(random.random() * (len(training_inputs) - WINDOW_LENGTH - FORECAST_HORIZON))
+        window = training_inputs[index:index + WINDOW_LENGTH].unsqueeze(1)
+        target = training_targets[index + FORECAST_HORIZON:index + WINDOW_LENGTH + FORECAST_HORIZON]
 
-    # Print stats
-    mse = loss.data[0]
+        # Zero gradients
+        optimizer.zero_grad()
+
+        # Update weights
+        outputs = model(window)
+        loss = criterion(outputs, target)
+        loss.backward()
+        optimizer.step()
+
+        # Print stats
+        mse = mse + loss.data[0]
+
     losses.append(mse)
-    print('Epoch: {0}/{1}, Loss: {2}'.format(epoch, EPOCHS, mse))
+    print('Epoch: {0}/{1},Training Loss: {2}'.format(epoch, EPOCHS, mse))
 
 
 # Generate date tag and path for outputs
